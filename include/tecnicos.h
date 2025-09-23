4af8bb3c83f47631e7e071f65f51416c7318be33
felipe_eduardo <felipe.fernandes.710@ufrn.edu.br>
2025-09-23 13:37:01 -0300

    chore: adicionados include guards (#ifndef/#define/#endif) nos headers

--- include/tecnicos.h
+++ include/tecnicos.h
@@ -1,5 +1,14 @@
+#ifndef TECNICOS_H
+#define TECNICOS_H
+
 void tecnicos();
+
 void cadastro_Tecnico();
+
 void atualizar_Tecnico();
+
 void pesquisar_Tecnico();
+
 void excluir_Tecnico();
+
+#endif
