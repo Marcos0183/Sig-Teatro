d4919b06c7063a577a81f93e4b8868a8b264c983
felipe_eduardo <felipe.fernandes.710@ufrn.edu.br>
2025-09-23 16:53:51 -0300

    fix: corrigindo bug na função pausar

--- utils.c
+++ utils.c
@@ -2,16 +2,20 @@
 #include <stdlib.h>
 #include "utils.h"
 
-void pausar() {
-    printf("\n Pressione Enter para continuar...");
-    while(getchar() != '\n');
-    getchar();
-}
 
 void limparBuffer() {
     int c;
     while ((c = getchar()) != '\n' && c != EOF);
 }
+
+
+
+void pausar() {
+    printf("\n Pressione Enter para continuar...");
+    limparBuffer();
+}
+
+
 
 void limparTela() {
     system("clear || cls");
