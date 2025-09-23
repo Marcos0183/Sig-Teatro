398704c8c726acc2705b03b4e548436a8cc1f053
felipe_eduardo <felipe.fernandes.710@ufrn.edu.br>
2025-09-23 13:27:13 -0300

    feat: adicionando o o crud utils.c

---
+++ utils.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "utils.h"
+
+void pausar() {
+    printf("\n Pressione Enter para continuar...");
+    while(getchar() != '\n');
+    getchar();
+}
+
+void limparBuffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
+
+void limparTela() {
+    system("clear || cls");
+}
