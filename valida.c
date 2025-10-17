56f0fdc630a62e4d821c405e66fafc880ce93539
felipe_eduardo <felipe.fernandes.710@ufrn.edu.br>
2025-10-17 17:27:09 -0300

    feat: adicionando o arquivo valida junto com a função valida_nome

---
+++ valida.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include "clientes.h"
+#include "leitura.h"
+#define true 1
+#define false 0
+
+
+int valida_nome(char *nome) {
+    int i;
+    int tamanho = strlen(nome);
+    if (tamanho < 3) {
+        printf("Nome muito curto. Deve ter pelo menos 3 caracteres.\n");
+        return false;
+    }
+    for (i = 0; i < tamanho; i++) {
+        if (!((nome[i] >= 'A' && nome[i] <= 'Z') || (nome[i] >= 'a' && nome[i] <= 'z') || nome[i] == ' ')) {
+            printf("Nome inválido. Use apenas letras e espaços.\n");
+            return false;
+        }
+    }
+    return true;
+}
