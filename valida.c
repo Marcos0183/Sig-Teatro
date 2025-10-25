c4149c0abd9a3fcf9dfcd6279e6b456c16523c1b
felipe_eduardo <felipe.fernandes.710@ufrn.edu.br>
2025-10-25 09:22:47 -0300

    feat: adicionando funcionalidade a função valida cpf

--- valida.c
+++ valida.c
@@ -27,10 +27,66 @@ int valida_nome(char *nome) {
 
 
 
-int valida_cpf(char *cpf) {
-    // Implementar a validação de CPF aqui
-    return true; // Placeholder
+int valida_cpf(char *cpf_raw) {
+    char cpf[20];
+    int i, j, soma, resto, dig1, dig2;
+
+    // Remove tudo que não for número
+    j = 0;
+    for (i = 0; cpf_raw[i] != '\0'; i++) {
+        if (cpf_raw[i] >= '0' && cpf_raw[i] <= '9') {
+            cpf[j++] = cpf_raw[i];
+        }
+    }
+    cpf[j] = '\0';
+
+    // Verifica se tem exatamente 11 dígitos
+    if (strlen(cpf) != 11) {
+        printf("CPF deve ter 11 dígitos.\n");
+        return 0;
+    }
+
+    // Rejeita CPFs com todos os dígitos iguais (ex: 11111111111)
+    // int iguais = 1;
+    //for (i = 1; i < 11; i++) {
+        // if (cpf[i] != cpf[0]) {
+        //    iguais = 0;
+        //  break;
+       // }
+    //}
+
+    //if (iguais) {
+        //printf("CPF com todos os dígitos iguais é inválido.\n");
+        //return 0;
+    //}
+
+    // Calcula o primeiro dígito verificador
+    soma = 0;
+    for (i = 0; i < 9; i++) {
+        soma += (cpf[i] - '0') * (10 - i);
+    }
+    resto = soma % 11;
+    dig1 = (resto < 2) ? 0 : 11 - resto;
+
+    // Calcula o segundo dígito verificador
+    soma = 0;
+    for (i = 0; i < 9; i++) {
+        soma += (cpf[i] - '0') * (11 - i);
+    }
+    soma += dig1 * 2;
+    resto = soma % 11;
+    dig2 = (resto < 2) ? 0 : 11 - resto;
+
+    // Compara com os dígitos originais
+    if (dig1 == (cpf[9] - '0') && dig2 == (cpf[10] - '0')) {
+        return 1; 
+    } else {
+        printf("CPF inválido! Dígitos verificadores incorretos.\n");
+        return 0; 
+    }
 }
+
+
 
 
 
@@ -100,7 +156,7 @@ int valida_email(char *email) {
         if (c == '.' && pos_arroba != -1){
             pos_ponto = i;
         }
-        
+
     }
 
     // Verifica se há um @ e um . depois do @
