c0f4821e2b2316095af29c6746aefd27a04539d7
felipe_eduardo <felipe.fernandes.710@ufrn.edu.br>
2025-10-30 19:05:06 -0300

    feat: adicionando ao header file todas as funções de relatorio

--- include/relatorio.h
+++ include/relatorio.h
@@ -6,4 +6,24 @@ void menu_relatorio();
 
 void relatorio();
 
+void relatorio_cliente();
+
+void relatorio_tecnico();
+
+void listar_clientes_ativos();
+
+void listar_clientes_inativos();
+
+void listar_clientes_por_nome();
+
+void listar_todos_clientes();
+
+void listar_tecnicos_ativos();
+
+void listar_tecnicos_inativos();
+
+void listar_tecnicos_por_nome();
+
+void listar_todos_tecnicos();
+
 #endif
