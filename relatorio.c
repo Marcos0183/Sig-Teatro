f4605bc61b064cc92ec0f37e8a462d58a5ef002e
felipe_eduardo <felipe.fernandes.710@ufrn.edu.br>
2025-10-30 17:44:40 -0300

    feat: adicionando o arquivo relatorio.c

--- relatorio.c
+++ relatorio.c
@@ -2,4 +2,37 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include "clientes.h"
+#include "anima.h"
+#include "utils.h"
+#include "clientes.h"
+#include "leitura.h"
+#define true 1
+#define false 0
 
+int tempo_cliente = 100;
+
+void menu_relatorio(){
+    limparTela();
+        printf("╔══════════════════════════════════════════════════╗\n");
+        func_Ani(tempo_cliente);
+        printf("║                MODULO RELATÓRIO                  ║\n");
+        func_Ani(tempo_cliente);
+        printf("╠══════════════════════════════════════════════════╣\n");
+        func_Ani(tempo_cliente);
+        printf("║                                                  ║\n");
+        func_Ani(tempo_cliente);
+        printf("║ ► 1. Relatório de Clientes                       ║\n");
+        func_Ani(tempo_cliente);
+        printf("║ ► 2. Relatório de Técnicos                      ║\n");
+        func_Ani(tempo_cliente);
+        printf("║                                                  ║\n");
+        func_Ani(tempo_cliente);
+        printf("╠══════════════════════════════════════════════════╣\n");
+        func_Ani(tempo_cliente);
+        printf("║ ► 0. Voltar ao Menu principal                    ║\n");
+        func_Ani(tempo_cliente);
+        printf("╚══════════════════════════════════════════════════╝\n");
+        func_Ani(tempo_cliente);
+        printf("--> Digite a opção desejada: ");
+}
