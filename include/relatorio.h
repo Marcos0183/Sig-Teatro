fd3ec52dbd75b5f8beb4a72cd971df73d7b61a58
felipe_eduardo <felipe.fernandes.710@ufrn.edu.br>
2025-10-31 17:35:10 -0300

    feat: adicionando ao header file novamente

---
+++ include/relatorio.h
@@ -0,0 +1,28 @@
+#ifndef RELATORIO_H
+#define RELATORIO_H
+
+void menu_relatorio();
+
+void relatorio();
+
+void relatorio_cliente();
+
+void listar_clientes_ativos();
+
+void listar_clientes_inativos();
+
+void listar_clientes_por_nome();
+
+void listar_todos_clientes();
+
+void relatorio_tecnico();
+
+void listar_tecnicos_ativos();
+
+void listar_tecnicos_inativos();
+
+void listar_tecnicos_por_nome();
+
+void listar_todos_tecnicos();
+
+#endif /* RELATORIO_H */
